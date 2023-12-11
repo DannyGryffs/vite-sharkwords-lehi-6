c376ee71e77b4746b8623bd8feee50ec2c2b268a
Daniel Griffin <dgriffinpv@gmail.com>
2023-12-11 16:41:29 -0700

    finished revealLetterInWord

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import './style.css';
+import getRandomWord from './src/randomWord.js';
+import { setSharkImage } from './src/sharkImage.js';
+import { setupWord } from './src/word.js';
+import { isLetterInWord } from './src/word.js';
+import { revealLetterInWord } from './src/word.js';
+
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -11,9 +17,11 @@ document.querySelector('#app').innerHTML = `
 `;
 
 const initSharkwords = () => {
+  const word = getRandomWord();
   let numWrong = 0;
-  const word = 'word';
-
+  setSharkImage(document.querySelector('#shark-img'), numWrong);
+  setupWord(document.querySelector('#word-container'), word);
+  
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
 };
--- src/word.js
+++ src/word.js
@@ -1,7 +1,32 @@
-let word;
+let word
 
-function setupWord(element, initWord) {}
+function setupWord(element, initWord) {
+    word = initWord
+    for (let i = 0; i < initWord.length; i++){
+        let letterDiv = document.createElement('div')
+        letterDiv.className = "letter-box"
+        element.appendChild(letterDiv)
+    }
+}
+// setupWord(document.querySelector('#word-container'), word);
 
-function isLetterInWord(letter) {}
+function isLetterInWord(letter) {
+    return word.includes(letter)
+}
 
-function revealLetterInWord(letter) {}
+function revealLetterInWord(letter) {
+    let elList = document.querySelectorAll('.letter-box')
+    for (let i = 0; i < elList.length; i++){
+        if(word[i] === letter){
+            elList[i].innerHTML = letter
+        }
+        //if the letter of word at i is equal to letter
+        
+        //then we need to take the respective underscore element
+        //and make it's innerHTML the letter
+    }
+
+
+}
+
+export {setupWord, isLetterInWord, revealLetterInWord}
